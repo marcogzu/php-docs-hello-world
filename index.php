efbfd60096e665e58b6181cc9abaf4c657900bbb
marcogzu <127275306+marcogzu@users.noreply.github.com>
2023-09-08 17:24:09 -0600

    otriri

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
 <?php
 
-echo "Aplicacion montada en PHP Azure Web, ya no mas Hola Mundo";
-echo "Prueba de Codigo en PHP";
-echo "Mas texto al codigo de muestra";
+echo "Aplicacion montada en PHP Azure Web, ya no mas Hola Mundo Cruel y Pecaminoso";
